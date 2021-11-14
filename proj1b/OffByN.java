baaca14a259b2a26637cebf678f024076ff964a6
lewamlyn <62939906+lewamlyn@users.noreply.github.com>
2021-11-14 15:57:23 +0800

    first finish proj1b

---
+++ proj1b/OffByN.java
@@ -0,0 +1,13 @@
+public class OffByN implements CharacterComparator {
+    private int diff;
+
+    OffByN(int N){
+        diff = N;
+    }
+
+    @Override
+    public boolean equalChars(char x, char y){
+        int z = x - y;
+        return (diff == z) || (diff == -z);
+    }
+}
---
+++ proj1b/Palindrome.java
@@ -0,0 +1,40 @@
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> d = new LinkedListDeque<>();
+        for (int i = 0; i < word.length(); i++) {
+            d.addLast(word.charAt(i));
+        }
+        return d;
+    }
+
+    private boolean isPalindromeDeque(Deque<Character> d) {
+        if (d.size() <= 1) {
+            return true;
+        }
+        if (d.removeFirst() != d.removeLast()) {
+            return false;
+        }
+        return isPalindromeDeque(d);
+    }
+
+    public boolean isPalindrome(String word) {
+        Deque d = wordToDeque(word);
+        return isPalindromeDeque(d);
+    }
+
+    private boolean isPalindromeDeque(Deque<Character> d, CharacterComparator cc) {
+        if (d.size() <= 1) {
+            return true;
+        }
+        if ( !cc.equalChars(d.removeFirst(), d.removeLast()))  {
+            return false;
+        }
+        return isPalindromeDeque(d,cc);
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        Deque d = wordToDeque(word);
+        return isPalindromeDeque(d,cc);
+    }
+
+}
